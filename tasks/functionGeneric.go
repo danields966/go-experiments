0612a3cc320480e6ebdfb525b70d1a6c44f6d502
Daniel Dushistov <ddanields966@gmail.com>
2024-02-21 20:54:39 +0300

    Added examples with generics

---
+++ tasks/functionGeneric.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	// ~ means addition of subtypes to specified type
+	~int | int32 | int64 | float32 | float64
+}
+
+func calcSum[T Number](a, b T) T {
+	var res T
+	res = a + b
+	return res
+}
+
+type Stack[T any] struct {
+	data []T
+}
+
+func (s *Stack[T]) Push(a T) {
+	s.data = append(s.data, a)
+}
+
+func (s *Stack[T]) Pop() T {
+	n := len(s.data) - 1
+	if n < 0 {
+		var res T
+		return res
+	}
+	res := s.data[n]
+	s.data = s.data[:n]
+	return res
+}
+
+type Set[T comparable] map[T]struct{}
+
+func NewSet[T comparable](elems ...T) Set[T] {
+	set := make(map[T]struct{}, len(elems))
+	for _, elem := range elems {
+		set[elem] = struct{}{}
+	}
+	return set
+}
+
+func (s Set[T]) Put(elem T) {
+	s[elem] = struct{}{}
+}
+func (s Set[T]) Has(elem T) bool {
+	_, has := s[elem]
+	return has
+}
+
+func main() {
+	fmt.Println("Testing generic functions")
+	fmt.Println(calcSum(1, 2))
+	fmt.Println(calcSum[int](1, 2)) // Use explicit type
+	fmt.Println(calcSum(1.5, 2.5))
+	fmt.Println(calcSum(1, 2.5))
+	fmt.Println(calcSum(1.5, 2))
+
+	fmt.Println("Testing generic types (Stack of any values")
+	stackAny := Stack[any]{}
+	stackAny.Push(1)
+	stackAny.Push(1.2)
+	stackAny.Push("asd")
+	fmt.Println(stackAny.Pop())
+	fmt.Println(stackAny.Pop())
+	fmt.Println(stackAny.Pop())
+	fmt.Println(stackAny.Pop())
+
+	fmt.Println("Testing Stack of int")
+	stackInt := Stack[int]{}
+	stackInt.Push(1)
+	stackInt.Push(2)
+	//stackInt.Push(1.2) // Error
+	fmt.Println(stackInt.Pop())
+	fmt.Println(stackInt.Pop())
+	fmt.Println(stackInt.Pop())
+
+	fmt.Println("Testing Set type")
+	set := NewSet("A", "B")
+	set.Put("C")
+	fmt.Println(set)
+	fmt.Println(set.Has("Z"))
+	fmt.Println(set.Has("C"))
+}
